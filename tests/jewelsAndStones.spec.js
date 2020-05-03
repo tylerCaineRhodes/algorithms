db28626ea4fc4955cf0d538198d53400b027982b
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-05-03 14:13:12 -0500

    Fix syntax in tests

--- tests/jewelsAndStones.spec.js
+++ tests/jewelsAndStones.spec.js
@@ -1,8 +1,8 @@
 const { numJewelsInStones } = require('../javascript/algorithms/jewelsAndStones.js');
 
 describe('jewels and stones', () => {
-  const answer1 = numJewelsInStones('Aa', 'AabbbbbA'),answer2 = numJewelsInStones('AaB', 'AabbBbbAaAaA');
-  
+  const answer1 = numJewelsInStones('Aa', 'AabbbbbA'), answer2 = numJewelsInStones('AaB', 'AabbBbbAaAaA');
+
   it('does a thing', () => {
     expect(answer1).toBe(3);
     expect(answer1).not.toBe(2);
