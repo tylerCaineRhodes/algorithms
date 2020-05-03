2b232b746252114cfe1222a50a419bb0f4dd87bd
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-05-03 15:42:28 -0500

    Write solution for inflight entertainment problem

---
+++ javascript/algorithms/inflightEntertainment.js
@@ -0,0 +1,15 @@
+const isEqualFlightLength = (flightLength, moviesArray) => {
+  let storage = {}, hasMovies = false;
+  
+  moviesArray.forEach((duration) => {
+    let secondDuration = flightLength - duration;
+    if(storage[secondDuration]){
+      hasMovies = true;
+    } else {
+      storage[duration] = true;
+    };
+  });
+  return hasMovies;
+};
+
+module.exports = {isEqualFlightLength};
---
+++ tests/inflightEntertainment.spec.js
@@ -0,0 +1,8 @@
+const { isEqualFlightLength } = require('../javascript/algorithms/inflightEntertainment.js');
+
+describe('inflightEntertainment', () => {
+  it('returns a boolean whether two movie runtimes have an exact sum of the time of flight', () => {
+    expect(isEqualFlightLength(60, [ 20, 20, 20, 50])).toBe(false);
+    expect(isEqualFlightLength(60, [ 20, 10, 15, 40])).toBe(true);
+  });
+});
