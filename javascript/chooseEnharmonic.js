a36916292dbb6e6f1cf15c8ddebe4b65a5ca2ba6
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-05-16 22:53:28 -0500

    Add new problem solutions

--- javascript/chooseEnharmonic.js
+++ javascript/chooseEnharmonic.js
@@ -2,7 +2,9 @@ const choosePitch = () => {
   const selection = ['A','A#', 'Bb', 'B', 'C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'F', 'F#', 'Gb', 'G'];
 
   let randomSelect = ~~(Math.random() * ~~(selection.length));
-  return selection[randomSelect];
+  console.log(selection[randomSelect]);
+
+  return;
 };
 
-console.log(choosePitch());
+choosePitch();
