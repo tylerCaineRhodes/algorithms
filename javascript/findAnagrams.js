c2d0b3892bc04eee9356ba4e62090af0ac21d45a
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-05-17 19:40:51 -0500

    Solve findAnagrams

---
+++ javascript/findAnagrams.js
@@ -0,0 +1,46 @@
+var findAnagrams = function (s, p) {
+  if (s.length < p.length) return [];
+  if(s === p) return [0];
+  let resultIndex = [];
+  let collectionP = counter(p);
+
+  for (let i = 0; i <= s.length - p.length; i++) {
+    let currentCollection = Object.assign({}, collectionP);
+      let counter = 0;
+      while (counter < p.length) {
+        if (currentCollection[s[i + counter]]) {
+          if (counter + 1 === p.length) {
+            resultIndex.push(i);
+            break;
+          }
+          if (currentCollection[s[i + counter]] === 1) {
+            delete currentCollection[s[i + counter]];
+          } else {
+            currentCollection[s[i + counter]] -= 1;
+          }
+          counter++;
+        } else {
+          break;
+        }
+      }
+  }
+  return resultIndex;
+};
+
+const counter = (str) => {
+  let collection = {};
+  for (let letter of str) {
+    if (collection[letter]) {
+      collection[letter] += 1;
+    } else {
+      collection[letter] = 1;
+    }
+  }
+  return collection;
+};
+
+
+
+console.log(findAnagrams('aacb', 'abc'))
+console.log(findAnagrams('cbaebabacd', 'abc'));
+console.log(findAnagrams('abab', 'ab'));
