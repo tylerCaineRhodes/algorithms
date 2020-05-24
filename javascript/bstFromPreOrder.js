3a09414e751147b659fafcd8b9daf2967bd09c12
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-05-24 15:36:23 -0500

    Solve bst from preorder

---
+++ javascript/bstFromPreOrder.js
@@ -0,0 +1,77 @@
+var bstFromPreorder = function (preorder) {
+  let tree = new TreeNode();
+
+  preorder.forEach(value => {
+    insert(tree, value)
+  })
+  return tree;
+};
+
+class TreeNode {
+  constructor(val, left, right){
+    this.val = val || null;
+    this.left = left || null;
+    this.right = right || null;
+  }
+}
+
+const insert = (tree, val) => {
+  if(!tree.val){
+    tree.val = val;
+    return;
+  }
+  if(val < tree.val){
+    if(!tree.left){
+      tree.left = new TreeNode(val)
+    } else {
+      insert(tree.left, val)
+    }
+  } else {
+    if(!tree.right){
+      tree.right = new TreeNode(val)
+    } else {
+      insert(tree.right, val)
+    }
+  }
+}
+
+const BFS = (node) => {
+  let collection = [node];
+  let seen = [];
+
+  while(collection.length){
+    let current = collection.shift()
+    seen.push(current.val);
+
+    if(current.left){
+      collection.push(current.left)
+    }
+    
+    if(current.right){
+      collection.push(current.right)
+    }
+  }
+  return seen;
+}
+
+const DFS = (node) => {
+  let collection = [node];
+  let seen = [];
+
+  while(collection.length){
+    let current = collection.shift()
+    seen.push(current.val);
+    
+    if(current.right){
+      collection.unshift(current.right)
+    }
+
+    if(current.left){
+      collection.unshift(current.left)
+    }
+  }
+  return seen;
+}
+let newTree = bstFromPreorder([8, 5, 1, 7, 10, 12]);
+
+console.log(DFS(newTree));
--- javascript/intervalIntersection.js
+++ javascript/intervalIntersection.js
@@ -2,7 +2,7 @@ var intervalIntersection = function (A, B) {
   //iterate over longest array followed by the shortest array;
   if(A.length === 0 || B.length === 0) return [];
   let longArr, shortArr, result = [];
-  
+
   if(A.length < B.length) {
     shortArr = A.slice();
     longArr = B.slice();
@@ -10,7 +10,7 @@ var intervalIntersection = function (A, B) {
     shortArr = B.slice();
     longArr = A.slice();
   }
-
+  
   for (let i = 0; i < longArr.length; i++) {
     for(let j = 0; j < shortArr.length; j++){
       let highestFirstIndex = Math.max(longArr[i][0], shortArr[j][0]);
@@ -24,18 +24,5 @@ var intervalIntersection = function (A, B) {
   return result;
 };
 
-const A = [
-    [0, 2],
-    [5, 10],
-    [13, 23],
-    [24, 25],
-  ],
-  B = [
-    [1, 5],
-    [8, 12],
-    [15, 24],
-    [25, 26],
-  ];
-
-  console.log(intervalIntersection(A, B));
+  console.log(intervalIntersection([[0, 2],[5, 10],[13, 23],[24, 25]],[[1, 5],[8, 12],[15, 24],[25, 26]]));
   console.log(intervalIntersection([[14, 16]], [[7,13], [16,26]]));
