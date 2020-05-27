d45d032c388958ad892758e93da7423d3fcee7e4
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-05-26 21:20:55 -0500

    Solve findMaxLength

--- javascript/findMaxLength.js
+++ javascript/findMaxLength.js
@@ -1,3 +1,4 @@
+//brute force
 var findMaxLength = function (nums) {
   let maxLen = 0;
 
@@ -16,6 +17,23 @@ var findMaxLength = function (nums) {
   } 
   return maxLen;
 };
+
+//using hashmap
+const findMaxLength = (nums) => {
+  let max = 0, curr = 0, collection = new Map();
+  collection.set(0, -1);
+
+  for(let i = 0; i < nums.length; i++){
+    curr += nums[i] === 0 ? -1 : nums[i];
+
+    if(collection.has(curr)){
+        max = Math.max(max, i - collection.get(curr));
+    } else {
+        collection.set(curr, i);
+    }
+  }
+  return max;
+}
 
 
 console.log(findMaxLength([0, 1])); //2
