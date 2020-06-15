1af79e06a67ba12b93bb8684d98f87d73cbd25a9
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-06-15 11:50:30 -0500

    Complete min heap

--- dataStructures/minHeap.js
+++ dataStructures/minHeap.js
@@ -1,32 +1,35 @@
 class MinHeap {
   constructor(arr) {
-    this.heap = arr || [null];
+    this.heap = arr || [];
   }
   getMin = () => {
-    return this.heap[1];
+    return this.heap[0];
   };
-  swapsies = (val1, val2) => {
-    [this.heap[val1], this.heap[val2]] = [this.heap[val2], this.heap[val1]];
+
+  //switches values in the heap
+  swapsies = (index1, index2) => {
+    [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
   };
+
   insert = (node) => {
+    //push to bottom of heap
     this.heap.push(node);
     if (this.heap.length > 1) {
+      //create pointer on inserted index
       let currentIndex = this.heap.length - 1;
-      while (
-        currentIndex > 1 &&
-        this.heap[~~(currentIndex / 2)] > this.heap[currentIndex]
-      ) {
-        [this.heap[currentIndex], this.heap[~~(currentIndex / 2)]] = [
-          this.heap[~~(currentIndex / 2)],
-          this.heap[currentIndex],
-        ];
-        currentIndex = ~~(currentIndex / 2);
+      //while pointerindex is greater than 1 and node at current index is greater than node at parent index
+      while (currentIndex > 0 && this.heap[~~((currentIndex -1) / 2)] > this.heap[currentIndex]) {
+        //swapies
+        this.swapsies(currentIndex, ~~((currentIndex - 1) / 2));
+        //set the pointer to parent's index
+        currentIndex = ~~((currentIndex - 1) / 2);
       }
     }
   };
 
   getKthSmallest = (k) => {
     if (k > this.heap.length - 1) return null;
+    //make immutable
     let copy = new MinHeap(this.heap);
     for (let i = 1; i < k; i++) {
       copy.remove();
@@ -37,30 +40,33 @@ class MinHeap {
   remove = () => {
     let len = this.heap.length;
 
-    if (len <= 1) return null;
-    if (len === 2) return this.heap.pop();
+    if (len <= 0) return null;
+    if (len === 1) return this.heap.pop();
 
-    let smallest = this.heap[1];
-
-    this.heap[1] = this.heap.pop();
-    let currentIndex = 1;
-    let [leftIndex, rightIndex] = [currentIndex * 2, currentIndex * 2 + 1];
-
-    while (
-      this.heap[leftIndex] &&
-      this.heap[rightIndex] &&
-      (this.heap[currentIndex] > this.heap[leftIndex] ||
-        this.heap[currentIndex] > this.heap[rightIndex])
-    ) {
+    //save smallest to return
+    let smallest = this.heap[0];
+    //set top of tree to the bottom value, in order to bubble down
+    this.heap[0] = this.heap.pop();
+    let currentIndex = 0;
+    //set left & right index to index of current's children
+    let [leftIndex, rightIndex] = [currentIndex * 2 + 1, currentIndex * 2 + 2];
+    //while both children exist in the array
+    while (this.heap[leftIndex] && this.heap[rightIndex] &&
+      //and  the current value is greater than either the left or the right
+      (this.heap[currentIndex] > this.heap[leftIndex] || this.heap[currentIndex] > this.heap[rightIndex])) {
+      //switch which ever is less than and reset the pointer to be that index
       if (this.heap[currentIndex] > this.heap[leftIndex]) {
         this.swapsies(currentIndex, leftIndex);
         currentIndex = leftIndex;
+
       } else if (this.heap[currentIndex] > this.heap[rightIndex]) {
         this.swapsies(currentIndex, rightIndex);
         currentIndex = rightIndex;
       }
-      [leftIndex, rightIndex] = [currentIndex * 2, currentIndex * 2 + 1];
+      //set the left and right pointers to point at the new children of current pointer
+      [leftIndex, rightIndex] = [currentIndex * 2 + 1, currentIndex * 2 + 2];
     }
+    //return smallest var
     return smallest;
   };
 }
@@ -70,6 +76,7 @@ test.insert(5);
 test.insert(4);
 test.insert(8);
 test.insert(1);
+test.remove();
 test.remove();
 test.insert(3);
 test.insert(16);
