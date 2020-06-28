c8b2515793d346b6c5f4aab766db7f5280aaa99a
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-06-27 22:46:15 -0500

    Adjust testing brackets

--- tests/findJudge.spec.js
+++ tests/findJudge.spec.js
@@ -1,6 +1,6 @@
 const { findJudge } = require('../javascript/findJudge.js');
 
-describe('find judge', () => [
+describe('find judge', () => {
   it('returns -1 when there is no judge', () => {
     expect(findJudge(3, [[1, 2],[2, 3]])).toBe(-1);
     expect(findJudge(3, [[1, 3],[2, 3],[3, 1]])).toBe(-1);
@@ -12,4 +12,4 @@ describe('find judge', () => [
     expect(findJudge(4, [[1, 2],[1, 3],[2, 1],[2, 3],[1, 4],[4, 3],[4, 1]])).toBe(3);
     expect(findJudge(2, [[1, 2]])).toBe(2);
   })
-]);
+});
