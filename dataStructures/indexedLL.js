d5c74e4a4894ce712a28e8239377bf1e4d6f1799
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-06-30 21:14:25 -0500

    Add tests for indexedLL

--- dataStructures/indexedLL.js
+++ dataStructures/indexedLL.js
@@ -10,51 +10,64 @@ class LL {
     this.head = null;
     this.tail = null;
   }
+
   addToHead = (val) => {
     let newNode = new Node(val);
 
     if(!this.head){
-      this.head = newNode
+      this.head = newNode;
+      this.tail = newNode;
     } else {
       let oldHead = this.head;
       this.head = newNode;
       newNode.next = oldHead;
     }
   };
+
   addToTail = (val) => {
     let newTail = new Node(val);
 
     if(!this.tail){
+      this.head = newTail
       this.tail = newTail
     } else {
-      list.tail.next = newTail;
-      list.tail = newTail
+      this.tail.next = newTail;
+      this.tail = newTail
     }
   }
+
   contains = (val) => {
     let current = this.head
     while(current){
-      if(current.value = val){
+      if(current.value === val){
         return true;
       }
       current = current.next;
     }
     return false;
   }
+
   getNodeAtIndex = (index) => {
     let current = this.head;
 
     for(let i = 0; i < index; i++){
+      if(current === null) return null;
+      
       current = current.next
     }
     return current;
   }
+
   removeHead = () => {
+    if(!this.head) return; 
+
     this.head = this.head.next; 
   }
+
   removeNode = (node) => {
     node.value = node.next.value;
     node.next = node.next.next;
   }
 }
 
+module.exports = { Node, LL }
---
+++ tests/indexedLL.spec.js
@@ -0,0 +1,92 @@
+const { Node, LL } = require('../dataStructures/indexedLL.js');
+
+describe('empty linked list', () => {
+  let linkedList;
+  beforeEach(() => {
+    linkedList = new LL();
+  })
+
+  it("addToHead: adds a head and a tail if either don't exist", () => {
+    linkedList.addToHead(1);
+
+    expect(linkedList.head.value).toEqual(1);
+    expect(linkedList.tail.value).toEqual(1);
+  });
+
+   it("addtoTail: adds a head and a tail if either don't exist, with method", () => {
+     linkedList.addToTail(1);
+
+     expect(linkedList.head.value).toEqual(1);
+     expect(linkedList.tail.value).toEqual(1);
+   });
+})
+
+describe('indexed linked list', () => {
+  let linkedList;
+  beforeEach(() => {
+    linkedList = new LL();
+       linkedList.addToHead(1);
+       linkedList.addToHead(2);
+       linkedList.addToHead(3);
+  });
+
+  it('addToHead: if head exists, adds a new value and sets to head of list', () => {
+    expect(linkedList.head.value).toEqual(3);
+    expect(linkedList.tail.value).toEqual(1);
+  })
+
+  it("addtoTail: if tail exists, adds a new value and sets to tail", () => {
+      linkedList.addToTail(5);
+
+      expect(linkedList.head.value).toEqual(3);
+      expect(linkedList.tail.value).toEqual(5);
+  });
+
+  it("contains: returns true if value is in linked list", () => {
+    expect(linkedList.contains(1)).toEqual(true);
+    expect(linkedList.contains(2)).toEqual(true);
+    expect(linkedList.contains(3)).toEqual(true);
+  });
+
+  it("contains: returns false if value is not in linked list", () => {
+    expect(linkedList.contains(6)).toEqual(false);
+  });
+
+  it('getNodeAtIndex: returns the proper node at given index', () => {
+    expect(linkedList.getNodeAtIndex(0).value).toEqual(3)
+    expect(linkedList.getNodeAtIndex(1).value).toEqual(2)
+    expect(linkedList.getNodeAtIndex(2).value).toEqual(1)
+    expect(linkedList.getNodeAtIndex(3)).toEqual(null)
+  });
+
+  it('removeHead: removes the head in the linked list', () => {
+    expect(linkedList.head.value).toEqual(3);
+    linkedList.removeHead();
+    linkedList.removeHead();
+    expect(linkedList.head.value).toEqual(1);
+  });
+
+  it('getNodeAtIndex: returns a given node at the proper index', () => {
+    expect(linkedList.getNodeAtIndex(0).value).toEqual(3)
+    expect(linkedList.getNodeAtIndex(2).value).toEqual(1);
+    expect(linkedList.getNodeAtIndex(5)).toEqual(null);
+  });
+
+  it('removeNode: removes a given node for the head', () => {
+    let removeVal = linkedList.getNodeAtIndex(0);
+    linkedList.removeNode(removeVal);
+    expect(linkedList.contains(3)).toEqual(false);
+  });
+
+  it('removeNode: removes a given node that isn\'t the head or tail val', () => {
+    let removeVal = linkedList.getNodeAtIndex(1);
+    linkedList.removeNode(removeVal);
+    expect(linkedList.contains(2)).toEqual(false);
+  });
+
+  it('removeNode: removes a given node for the tail', () => {
+    let removeVal = linkedList.getNodeAtIndex(2);
+    linkedList.removeNode(removeVal);
+    expect(linkedList.contains(1)).toEqual(false);
+  });
+});
