efbeccfa20fc442be2f40e2da207af614ba48c67
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-06-30 20:04:42 -0500

    Solution for removeKthElement

---
+++ javascript/removeKthToLastNode.js
@@ -0,0 +1,36 @@
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function removeKthNodeFromEnd1(head, k) {
+  let pointer = head,
+    disciple = head;
+  let increment = 1;
+
+  while (pointer.next !== null) {
+    if (increment > k) {
+      pointer = pointer.next;
+      disciple = disciple.next;
+      increment++;
+      continue;
+    }
+    pointer = pointer.next;
+    increment++;
+  }
+  if (increment <= k) {
+    head.value = head.next.value;
+    head.next = head.next.next;
+  } else {
+    disciple.next = disciple.next.next;
+  }
+}
