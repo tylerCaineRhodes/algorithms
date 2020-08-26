024b68aa8e57d0433cdc243e845a182c65064998
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-08-26 16:39:31 -0500

    Add solution to longestPeak

---
+++ tests/longestPeak.spec.js
@@ -0,0 +1,13 @@
+const { longestPeak } = require('../Javascript/longestPeak');
+
+describe('longestpeak', () => {
+  it('returns longest peak of two', () => {
+    expect(longestPeak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3])).toBe(6);
+  });
+
+  it('returns 0 when there is no peak or array is empty', () => {
+    expect(longestPeak([1, 2, 3, 4, 5, 6, 10, 100, 1000])).toBe(0);
+
+    expect(longestPeak([])).toBe(0);
+  });
+});
