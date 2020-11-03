ef0112634b835ea75bdc6ca07f8ac759532b2a01
Tyler Rhodes <rhodetyl000@gmail.com>
2020-11-03 12:40:03 -0600

    Add solution to climb stairs

---
+++ javascript/climbStairs.js
@@ -0,0 +1,49 @@
+function climbStairs(n) {
+  return getNumberOfClimbs(0, n)
+}
+
+function getNumberOfClimbs(step, n) {
+  const stepIsOutOfBounds = step > n;
+  const isAtLastStep = step === n;
+
+  if (stepIsOutOfBounds) {
+    return 0;
+  }
+  if(isAtLastStep) {
+    return 1;
+  }
+  return getNumberOfClimbs(step + 1, n) + getNumberOfClimbs(step + 2, n);
+}
+
+function climbStairsWithMemo(n) {
+  const memo = {}
+  return getNumberOfClimbsWithMemo(0, n, memo)
+}
+
+function getNumberOfClimbsWithMemo(step, n, memo) {
+  const stepIsOutOfBounds = step > n;
+  const isAtLastStep = step === n;
+
+  if (stepIsOutOfBounds) {
+    return 0;
+  }
+  if(isAtLastStep) {
+    return 1;
+  }
+
+  if(memo[step] > 0) {
+    return memo[step]
+  }
+  memo[step] = getNumberOfClimbsWithMemo(step + 1, n, memo) + getNumberOfClimbsWithMemo(step + 2, n, memo)
+  return memo[step];
+}
+
+function climbStairsIII(n) {
+  const numberOfSteps = new Array(n).fill(0);
+ [numberOfSteps[0], numberOfSteps[1]] = [1, 2];
+
+ for(let i = 3; i < numberOfSteps.length; i++) {
+  numberOfSteps[i] = numberOfSteps[i - 1] + numberOfSteps[i  - 2];
+ }
+ return numberOfSteps[numberOfSteps.length - 1]
+}
