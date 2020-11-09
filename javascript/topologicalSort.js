56f11a6c145d835fc43b25a29a2c11c50a1fe52d
Tyler Rhodes <rhodetyl000@gmail.com>
2020-11-09 17:48:57 -0600

    Add first solution for topological sort

---
+++ javascript/topologicalSort.js
@@ -0,0 +1,77 @@
+//depth first search solution
+function topologicalSort(jobs, deps) {
+  const jobGraph = createJobGraph(jobs, deps);
+  return getOrderedJobs(jobGraph);
+}
+
+function getOrderedJobs(graph) {
+  const orderedJobs = [];
+  const { nodes } = graph;
+  while (nodes.length) {
+    const node = nodes.pop();
+    const containsCycle = depthFirstTraverse(node, orderedJobs);
+    if (containsCycle) {
+      return [];
+    }
+  }
+  return orderedJobs;
+}
+
+function depthFirstTraverse(node, orderedJobs) {
+  if (node.visited) return false;
+  if (node.visiting) return true;
+  node.visiting = true;
+  for (let prereqNode of node.prereqs) {
+    const containsCycle = depthFirstTraverse(prereqNode, orderedJobs);
+    if (containsCycle) return true;
+  }
+  node.visited = true;
+  node.visiting = false;
+  orderedJobs.push(node.job);
+  return false;
+}
+
+function createJobGraph(jobs, deps) {
+  const graph = new JobGraph(jobs);
+
+  for (const [prereq, job] of deps) {
+    graph.addPrereq(job, prereq);
+  }
+  return graph;
+}
+
+class JobGraph {
+  constructor(jobs) {
+    this.nodes = [];
+    this.graph = {};
+    for (let job of jobs) {
+      this.addNode(job);
+    }
+  }
+  addNode(job) {
+    this.graph[job] = new JobNode(job);
+    this.nodes.push(this.graph[job]);
+  }
+
+  addPrereq(job, prereq) {
+    const jobNode = this.getNode(job);
+    const prereqNode = this.getNode(prereq);
+    jobNode.prereqs.push(prereqNode);
+  }
+
+  getNode(job) {
+    if (!(job in this.graph)) {
+      this.addNode(job);
+    }
+    return this.graph[job];
+  }
+}
+
+class JobNode {
+  constructor(job) {
+    this.job = job;
+    this.prereqs = [];
+    this.visited = false;
+    this.visiting = false;
+  }
+}
