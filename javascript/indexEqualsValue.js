fc0f17f47f017e671e1b9b229e9861dac2409a7a
Tyler Rhodes <rhodetyl000@gmail.com>
2020-12-03 20:17:52 -0600

    Add more optimal solution to index equals val

--- javascript/indexEqualsValue.js
+++ javascript/indexEqualsValue.js
@@ -17,3 +17,25 @@ function indexEqualsValue(array) {
   }
   return firstIdx === Infinity ? -1 : array[firstIdx];
 }
+
+function indexEqualsValue(array) {
+  let p1 = 0;
+  let p2 = array.length - 1;
+
+  while (p1 <= p2) {
+    let mid = ~~((p1 + p2) / 2);
+    if (array[mid] === mid) {
+      if (mid === 0 || array[mid - 1] < mid - 1) {
+        return array[mid];
+      } else {
+        p2 = mid - 1;
+      }
+    } else if (array[mid] < mid) {
+      p1 = mid + 1;
+    } else {
+      p2 = mid - 1;
+    }
+  }
+  return -1;
+}
+
