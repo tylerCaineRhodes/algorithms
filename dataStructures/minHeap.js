c806d464aa1f3a69f106c7949d88470e0716ff65
Tyler Rhodes <rhodetyl000@gmail.com>
2020-12-08 14:02:09 -0600

    Add cleaner solution to minHeap

--- dataStructures/minHeap.js
+++ dataStructures/minHeap.js
@@ -72,6 +72,78 @@ class MinHeap {
   };
 }
 
+
+class MinHeap {
+  constructor(array) {
+    this.heap = this.buildHeap(array);
+  }
+
+  buildHeap(array) {
+    const lastParent = Math.floor(array.length - 2 / 2);
+    for (let i = lastParent; i >= 0; i--) {
+      this.siftDown(i, array.length - 1, array);
+    }
+    return array;
+  }
+
+  siftDown(currIdx, endIdx, heap) {
+    let left = currIdx * 2 + 1;
+    while (left <= endIdx) {
+      let right = currIdx * 2 + 2 <= endIdx ? currIdx * 2 + 2 : -1;
+      let toSwap;
+      if (right !== -1) {
+        if (heap[right] < heap[left]) {
+          toSwap = right;
+        } else {
+          toSwap = left;
+        }
+      } else {
+        toSwap = left;
+      }
+      if (heap[toSwap] < heap[currIdx]) {
+        this.swap(currIdx, toSwap, heap);
+        currIdx = toSwap;
+        left = currIdx * 2 + 1;
+      } else {
+        return;
+      }
+    }
+  }
+
+  siftUp(currIdx, heap) {
+    let parent = Math.floor((currIdx - 1) / 2);
+    while (currIdx > 0) {
+      if (heap[currIdx] < heap[parent]) {
+        this.swap(currIdx, parent, heap);
+        currIdx = parent;
+        parent = Math.floor((currIdx - 1) / 2);
+      } else {
+        return;
+      }
+    }
+  }
+  swap(i, j, heap) {
+    [heap[i], heap[j]] = [heap[j], heap[i]];
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+
+  remove() {
+    this.swap(0, this.heap.length - 1, this.heap);
+    const removed = this.heap.pop();
+    this.siftDown(0, this.heap.length - 1, this.heap);
+    return removed;
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    this.siftUp(this.heap.length - 1, this.heap);
+  }
+}
+
+
 let test = new MinHeap();
 test.insert(5);
 test.insert(4);
