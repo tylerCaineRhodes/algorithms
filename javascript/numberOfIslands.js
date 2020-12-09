f6631d5d3ca8fbc9ec61e42e8efbabebe038b7e7
Tyler Rhodes <rhodetyl000@gmail.com>
2020-12-09 14:05:42 -0600

    Add new solutions to num of islands

--- javascript/numberOfIslands.js
+++ javascript/numberOfIslands.js
@@ -19,36 +19,90 @@ function dfs(grid, r, c) {
   if (c < grid.length - 1 && grid[r][c + 1] === '1') dfs(grid, r, c + 1);
 }
 
-var numIslands = function (grid) {
+var numIslandsII = function (grid) {
   let numberOfIslands = 0;
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
-      if (grid[i][j] === '1') {
-        numberOfIslands++;
-        grid[i][j] = '0';
-        const queue = [[i, j]];
-        while (queue.length) {
-          const [i, j] = queue.shift();
-
-          const neighbors = getNeighbors(i, j, grid);
-          for (const [row, col] of neighbors) {
-            grid[row][col] = '0';
-            queue.push([row, col]);
-          }
-        }
-      }
+      if (grid[i][j] === '0') continue;
+      numberOfIslands++;
+      traverse(i, j, grid);
     }
   }
   return numberOfIslands;
 };
 
-function getNeighbors(row, col, grid, seen) {
+function traverse(i, j, grid) {
+  grid[i][j] = '0';
+  const queue = [[i, j]];
+  while (queue.length) {
+    const [i, j] = queue.shift();
+
+    const neighbors = getNeighbors(i, j, grid);
+    for (const [row, col] of neighbors) {
+      grid[row][col] = '0';
+      queue.push([row, col]);
+    }
+  }
+}
+
+
+function getNeighborsII(row, col, grid) {
   const neighbors = [];
   if (row > 0 && grid[row - 1][col] === '1') neighbors.push([row - 1, col]);
   if (row < grid.length - 1 && grid[row + 1][col] === '1')
     neighbors.push([row + 1, col]);
   if (col > 0 && grid[row][col - 1] === '1') neighbors.push([row, col - 1]);
   if (col < grid[0].length - 1 && grid[row][col + 1] === '1')
+    neighbors.push([row, col + 1]);
+  return neighbors;
+}
+
+
+var numIslandsIII = function (grid) {
+  const seen = grid.map((row) => row.map((val) => false));
+  let numberOfIslands = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === '0') continue;
+      if (seen[i][j]) continue;
+      numberOfIslands++;
+      traverse(i, j, grid, seen);
+    }
+  }
+  return numberOfIslands;
+};
+
+function traverse(i, j, grid, seen) {
+  seen[i][j] = true;
+  const queue = [[i, j]];
+  while (queue.length) {
+    const [i, j] = queue.shift();
+
+    const neighbors = getNeighbors(i, j, grid, seen);
+    for (const [row, col] of neighbors) {
+      seen[row][col] = true;
+      queue.push([row, col]);
+    }
+  }
+}
+
+function getNeighborsIII(row, col, grid, seen) {
+  const neighbors = [];
+  if (row > 0 && grid[row - 1][col] === '1' && !seen[row - 1][col])
+    neighbors.push([row - 1, col]);
+  if (
+    row < grid.length - 1 &&
+    grid[row + 1][col] === '1' &&
+    !seen[row + 1][col]
+  )
+    neighbors.push([row + 1, col]);
+  if (col > 0 && grid[row][col - 1] === '1' && seen[row][col - 1] === false)
+    neighbors.push([row, col - 1]);
+  if (
+    col < grid[0].length - 1 &&
+    grid[row][col + 1] === '1' &&
+    !seen[row][col + 1]
+  )
     neighbors.push([row, col + 1]);
   return neighbors;
 }
