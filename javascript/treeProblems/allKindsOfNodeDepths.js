2b0b65400dd35c299e28c494c1744432c4f2503c
Tyler Rhodes <rhodetyl000@gmail.com>
2020-12-14 14:05:50 -0600

    Add solution to all kinds of node depths

---
+++ javascript/treeProblems/allKindsOfNodeDepths.js
@@ -0,0 +1,26 @@
+function allKindsOfNodeDepths(root) {
+  let allNodeDepths = 0;
+  const queue = [root];
+  while (queue.length) {
+    const node = queue.shift();
+    if (!node) continue;
+    allNodeDepths += countNodeDepths(node);
+    queue.push(node.left);
+    queue.push(node.right);
+  }
+  return allNodeDepths;
+}
+
+function countNodeDepths(tree, depth = 0) {
+  if (!tree) return 0;
+  return (depth + countNodeDepths(tree.left, depth + 1) + countNodeDepths(tree.right, depth + 1));
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
