4ca0e21a1028ed71488565da21213e9950ea9eb8
Tyler Rhodes <rhodetyl000@gmail.com>
2021-05-05 18:25:53 -0500

    Add solution for merge overlapping intervals

---
+++ javascript/mergeOverlappingIntervals.js
@@ -0,0 +1,18 @@
+function mergeOverlappingIntervals(array) {
+  array.sort((a, b) => a[0] - b[0])
+  const merged = [array[0]]
+
+  let curr = merged[0]
+  for(let i = 1; i < array.length; i++) {
+    const [currFirst, currLast] = curr;
+    const [nextFirst, nextLast] = array[i];
+
+    if(currLast < nextFirst) {
+      merged.push(array[i])
+      curr = merged[merged.length - 1]
+    } else {
+      curr[1] = Math.max(currLast, nextLast)
+    }
+  }
+  return merged
+}
