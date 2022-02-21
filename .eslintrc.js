2302c9ab701aa6085b32c89228ced05267d05ff3
Tyler Rhodes <rhodetyl000@gmail.com>
2022-02-20 18:42:39 -0600

    Configure es-lint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['off'],
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': ['off'],
+    '@typescript-eslint/adjacent-overload-signatures': ['off'],
+  },
+};
