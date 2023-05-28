1b8accbedf2be7f57d3d641a9bf8b6ff19517a3d
Tyler Rhodes <rhodetyl000@gmail.com>
2023-05-28 08:16:35 -0700

    Add solutions for riverSizes

--- src/riverSizes.js
+++ src/riverSizes.js
@@ -1,51 +1,50 @@
 function riverSizes(matrix) {
-  let visited = matrix.map(row => row.map(val => false));
-  let result = [];
-  for(let i = 0; i < matrix.length; i++){
-    for(let j = 0; j < matrix[i].length; j++){
-      if(visited[i][j]) continue;
-      traverseNode(matrix, i, j, visited, result);
+  const riverSizes = [];
+  const visited = new Array(matrix.length)
+    .fill()
+    .map(() => new Array(matrix[0].length).fill(false));
+
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[0].length; c++) {
+      if (visited[r][c]) continue;
+      if (!matrix[r][c]) continue;
+      traverseRivers(matrix, r, c, visited, riverSizes);
     }
   }
-  return result;
+  return riverSizes;
 }
 
-const traverseNode = (grid, i, j, aux, result) => {
-  let currRiverSize = 0; 
-  const unvisitedQueue= [[i, j]];
+function traverseRivers(matrix, r, c, visited, riverSizes) {
+  let count = 0;
 
-  while(unvisitedQueue.length){
-    let [row, col] = unvisitedQueue.pop();
-    if(aux[row][col]) continue;
-    aux[row][col] = true;
-    if(grid[row][col] === 0) continue;
-    currRiverSize++;
-    let unvisitedNodes = getUnvisited(row, col, grid, aux);
-    for(let nodePair of unvisitedNodes){
-      unvisitedQueue.push(nodePair);
+  const queue = [[r, c]];
+  while (queue.length) {
+    const [r, c] = queue.shift();
+
+    visited[r][c] = true;
+    count++;
+
+    for (const neighbor of getNeighbors(matrix, r, c, visited)) {
+      const [r, c] = neighbor;
+      if (matrix[r][c] === 0) continue;
+      if (visited[r][c]) continue;
+
+      queue.push(neighbor);
     }
   }
-  if(currRiverSize > 0){
-    result.push(currRiverSize);
+
+  if (count > 0) {
+    riverSizes.push(count);
   }
 }
 
-const getUnvisited = (row, col, matrix, aux) => {
-  let unvisited = [];
-  
-  if(row > 0 && !aux[row - 1][col]){
-    unvisited.push([row - 1, col])
-  }
-  if(row < matrix.length - 1 && !aux[row + 1][col]){
-    unvisited.push([row + 1, col])
-  }
-  if(col > 0 && !aux[row][col - 1]){
-    unvisited.push([row, col - 1])
-  }
-  if(col < matrix[row].length - 1 && !aux[row][col + 1]){
-    unvisited.push([row, col + 1])
-  }
-  return unvisited;
+function getNeighbors(matrix, r, c) {
+  const neighbors = [];
+  if (r > 0) neighbors.push([r - 1, c]);
+  if (c > 0) neighbors.push([r, c - 1]);
+  if (c < matrix[0].length - 1) neighbors.push([r, c + 1]);
+  if (r < matrix.length - 1) neighbors.push([r + 1, c]);
+  return neighbors;
 }
 
 const testMatrix1 = [
@@ -58,3 +57,101 @@ const testMatrix1 = [
 const testMatrix2 = [[1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0]];
 console.log(riverSizes(testMatrix1));
 console.log(riverSizes(testMatrix2));
+
+function riverSizes(matrix) {
+  const riverSizes = [];
+  const visited = new Array(matrix.length)
+    .fill()
+    .map(() => new Array(matrix[0].length).fill(false));
+
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[0].length; c++) {
+      if (visited[r][c]) continue;
+      if (!matrix[r][c]) {
+        visited[r][c] = true;
+        continue;
+      }
+      traverseRivers(matrix, r, c, visited, riverSizes);
+    }
+  }
+  return riverSizes;
+}
+
+function traverseRivers(matrix, r, c, visited, riverSizes) {
+  let count = 0;
+
+  const queue = [[r, c]];
+  while (queue.length) {
+    const [r, c] = queue.shift();
+
+    if (visited[r][c]) continue;
+    visited[r][c] = true;
+
+    if (matrix[r][c] === 0) continue;
+    count++;
+
+    for (const neighbor of getNeighbors(matrix, r, c, visited)) {
+      queue.push(neighbor);
+    }
+  }
+
+  if (count > 0) {
+    riverSizes.push(count);
+  }
+}
+
+function getNeighbors(matrix, r, c) {
+  const unvisited = [];
+  if (r > 0) unvisited.push([r - 1, c]);
+  if (c > 0) unvisited.push([r, c - 1]);
+  if (c < matrix[0].length - 1) unvisited.push([r, c + 1]);
+  if (r < matrix.length - 1) unvisited.push([r + 1, c]);
+  return unvisited;
+}
+
+function riverSizes(matrix) {
+  const seen = Array.from({ length: matrix.length }, () =>
+    new Array(matrix[0].length).fill(false)
+  );
+  const riverSizes = [];
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[0].length; c++) {
+      if (seen[r][c]) continue;
+      if (matrix[r][c] === 0) {
+        seen[r][c] = true;
+        continue;
+      }
+      traverseRiver(matrix, r, c, seen, riverSizes);
+    }
+  }
+  return riverSizes;
+}
+
+function traverseRiver(matrix, r, c, seen, riverSizes) {
+  const queue = [[r, c]];
+  let count = 0;
+  while (queue.length) {
+    const [r, c] = queue.pop();
+
+    seen[r][c] = true;
+    if (matrix[r][c] === 0) continue;
+
+    count++;
+    for (const neighbor of getUnvisitedNeighbors(matrix, r, c, seen)) {
+      queue.push(neighbor);
+    }
+  }
+
+  if (count > 0) {
+    riverSizes.push(count);
+  }
+}
+
+function getUnvisitedNeighbors(matrix, r, c, seen) {
+  const neighbors = [];
+  if (r > 0 && !seen[r - 1][c]) neighbors.push([r - 1, c]);
+  if (c > 0 && !seen[r][c - 1]) neighbors.push([r, c - 1]);
+  if (c < matrix[0].length - 1 && !seen[r][c + 1]) neighbors.push([r, c + 1]);
+  if (r < matrix.length - 1 && !seen[r + 1][c]) neighbors.push([r + 1, c]);
+  return neighbors;
+}
