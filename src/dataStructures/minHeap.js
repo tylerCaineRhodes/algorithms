4d160ce68ec4ea29b82f104fe2022d57057c7d83
Tyler Rhodes <rhodetyl000@gmail.com>
2023-05-31 14:37:07 -0700

    Reformat for clarity

--- src/dataStructures/minHeap.js
+++ src/dataStructures/minHeap.js
@@ -8,31 +8,32 @@ class MinHeap {
   };
 
   swapsies = (index1, index2) => {
-    [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
+    [this.heap[index1], this.heap[index2]] = [
+      this.heap[index2],
+      this.heap[index1],
+    ];
   };
 
-  insert = (node) => {
-    //push to bottom of heap
+  insert(node) {
     this.heap.push(node);
-    if (this.heap.length > 1) {
-      //create pointer on inserted index
-      let currentIndex = this.heap.length - 1;
-      let parentIndex = ~~((currentIndex - 1) / 2);
-      //while pointerindex is greater than 1 and node at current index is greater than node at parent index
-      while (currentIndex > 0 && this.heap[parentIndex] > this.heap[currentIndex]) {
-        //swapies
-        this.swapsies(currentIndex, parentIndex);
-        //set the pointer to parent's index
-        currentIndex = parentIndex;
-        parentIndex = ~~((currentIndex - 1) / 2);
-      }
+    let currentIndex = this.heap.length - 1;
+    let parentIndex = ~~((currentIndex - 1) / 2);
+
+    while (
+      currentIndex > 0 &&
+      this.heap[parentIndex] > this.heap[currentIndex]
+    ) {
+      this.swapsies(currentIndex, parentIndex);
+      currentIndex = parentIndex;
+      parentIndex = ~~((currentIndex - 1) / 2);
     }
-  };
+  }
 
   getKthSmallest = (k) => {
     if (k > this.heap.length - 1) return null;
-    let copyArr = this.heap.slice();
-    let copyHeap = new MinHeap(copyArr);
+    const copyArr = this.heap.slice();
+    const copyHeap = new MinHeap(copyArr);
+
     for (let i = 1; i < k; i++) {
       copyHeap.remove();
     }
@@ -40,38 +41,36 @@ class MinHeap {
   };
 
   remove = () => {
-    let len = this.heap.length;
+    const len = this.heap.length;
     if (len <= 0) return null;
     if (len === 1) return this.heap.pop();
-    //save smallest to return
-    let smallest = this.heap[0];
-    //set top of tree to the bottom value, in order to bubble down
-    this.heap[0] = this.heap.pop();
-    let currentIndex = 0;
-    //left index is (currIndex * 2)+ 1
-    let leftChildIndex = (currentIndex * 2) + 1;
-    //while left child <= last index
-    while(leftChildIndex < len) {
-      //if rightChild is <= last index, it is (currIndex * 2) + 2, else it's -1
-      let rightChildIndex = (currentIndex * 2) + 2 < len ? (currentIndex * 2) + 2 : - 1;
-      let indexToSwap;
 
-      if(rightChildIndex !== -1 && this.heap[rightChildIndex] < this.heap[leftChildIndex]) {
-        indexToSwap = rightChildIndex;
-      } else {
-        indexToSwap = leftChildIndex;
-      }
-      if(this.heap[currentIndex] < this.heap[indexToSwap]) {
+    const smallest = this.heap[0];
+    this.heap[0] = this.heap.pop();
+
+    let currentIndex = 0;
+    let leftChildIndex = currentIndex * 2 + 1;
+
+    while (leftChildIndex < len) {
+      const rightChildIndex =
+        currentIndex * 2 + 2 < len ? currentIndex * 2 + 2 : -1;
+
+      const indexToSwap =
+        rightChildIndex !== -1 &&
+        this.heap[rightChildIndex] < this.heap[leftChildIndex]
+          ? rightChildIndex
+          : leftChildIndex;
+
+      if (this.heap[indexToSwap] < this.heap[currentIndex]) {
         this.swapsies(currentIndex, indexToSwap);
         currentIndex = indexToSwap;
-        leftChildIndex = (currentIndex * 2) + 1;
+        leftChildIndex = currentIndex * 2 + 1;
       } else {
         return smallest;
       }
     }
   };
 }
-
 
 class MinHeapI {
   constructor(array) {
@@ -142,7 +141,6 @@ class MinHeapI {
     this.siftUp(this.heap.length - 1, this.heap);
   }
 }
-
 
 let test = new MinHeap();
 test.insert(5);
