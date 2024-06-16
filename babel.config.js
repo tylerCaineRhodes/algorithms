28b49082f386598ab99dcffb87ed75ec7f1a0ea7
Tyler Rhodes <rhodetyl000@gmail.com>
2024-06-16 12:43:41 -0700

    Opt-in to decorators and exp pipeline operators

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  presets: ["@babel/preset-env", "@babel/preset-typescript"],
+  plugins: [
+    "@babel/plugin-proposal-decorators", { version: "2023-11" },
+    "@babel/plugin-proposal-pipeline-operator", { "topicToken": "^^" }
+  ],
 };
