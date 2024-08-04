7942e90ed34b03527b32d5d6bd68ed0b0c9c04ed
Tyler Rhodes <rhodetyl000@gmail.com>
2024-08-04 16:53:23 -0700

    Opt in to experimental ECMA features

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-typescript"],
+  presets: ['@babel/preset-env', '@babel/preset-typescript'],
   plugins: [
-    "@babel/plugin-proposal-decorators", { version: "2023-11" },
-    "@babel/plugin-proposal-pipeline-operator", { "topicToken": "^^" }
+    ['@babel/plugin-proposal-decorators', { version: '2023-11' }],
+    [
+      '@babel/plugin-proposal-pipeline-operator',
+      { topicToken: '^^', proposal: 'hack' },
+    ],
   ],
 };
